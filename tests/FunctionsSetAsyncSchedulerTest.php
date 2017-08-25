0b2dcefc8638984e1c161dc422951ef18e0d2b63
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-08-26 00:24:03 +0200

    New function setAsyncScheduler to set an async scheduler based on the reactphp event loop

---
+++ tests/FunctionsSetAsyncSchedulerTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Tools\Rx;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use React\EventLoop\Factory;
+use Rx\Scheduler;
+use function ApiClients\Tools\Rx\setAsyncScheduler;
+
+final class FunctionsSetAsyncSchedulerTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSetAsyncScheduler()
+    {
+        $loop = Factory::create();
+        setAsyncScheduler($loop);
+        $scheduler = Scheduler::getAsync();
+        setAsyncScheduler($loop);
+        self::assertSame($scheduler, Scheduler::getAsync());
+    }
+}
